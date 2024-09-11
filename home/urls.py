3c716439325f0a051ba7d61c3d634c9fe6d25564
Paul <mwapaul265@gmail.com>
2024-09-11 18:01:12 +0300

    Django Auth

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
-from .views import homePage
+from django.urls import path
+from .views import homePage, sign_up_page, login_page, profile_page
 
 urlpatterns = [
     path('', homePage, name='home'),
+    path('signup/', sign_up_page, name='signUp'),
+    path('login/', login_page, name='login'),
+    path('myprofile/', profile_page, name='profile'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import create_user_form
 
 # Create your views here.
 def homePage(request):
     return render(request, 'home.html')
+def sign_up_page(request):
+    form = create_user_form()
+    if request.method == 'POST':
+        form = create_user_form(request.post)
+        if form.is_valid():
+            form.save()
+    return render(request, 'signUp.html', {'form':form})
+def login_page(request):
+    return render(request, 'login.html')
+def profile_page(request):
+    return render(request, 'profile.html')
